718555e77a5b048d2fe68cde4d057bc5334c7598
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-09-09 21:34:47 +0200

    Added __manifest__.py

---
+++ __manifest__.py
@@ -0,0 +1,8 @@
+{
+'name': 'Library Management',
+'description': 'Manage library book catalogue and lending.',
+'author': 'Ahmed',
+'depends': ['base'],
+'application': True,
+'data': [ 'security/library_security.xml', 'security/ir.model.access.csv', 'views/library_menu.xml', 'views/book_view.xml', ],
+}
