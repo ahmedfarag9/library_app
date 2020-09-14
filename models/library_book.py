6b11b2db93e38bc370f369d4662e9054e8f760c4
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-09-14 15:39:46 +0200

    removed @api.multi from code

--- models/library_book.py
+++ models/library_book.py
@@ -12,7 +12,6 @@ class Book(models.Model):
 	publisher_id = fields.Many2one('res.partner', string='Publisher')
 	author_ids = fields.Many2many('res.partner', string='Authors')
 
-	@api.multi
 	def _check_isbn(self):
 		self.ensure_one()
 		digits = [int(x) for x in self.isbn if x.isdigit()]
@@ -23,7 +22,6 @@ class Book(models.Model):
 			check = 10 - remain if remain != 0 else 0
 			return digits[-1] == check
 
-	@api.multi
 	def button_check_isbn(self):
 		for book in self:
 			if not book.isbn:
