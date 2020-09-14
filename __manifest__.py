d81771339b94c856080b776656f5c71b2d21f1d7
ahmedfarag9 <ahmedfarag.a95@gmail.com>
2020-09-14 18:36:11 +0200

    Added new xml template

--- __manifest__.py
+++ __manifest__.py
@@ -4,5 +4,5 @@
 'author': 'Ahmed',
 'depends': ['base'],
 'application': True,
-'data': [ 'security/library_security.xml', 'security/ir.model.access.csv', 'views/library_menu.xml', 'views/book_view.xml', ],
+'data': [ 'security/library_security.xml', 'security/ir.model.access.csv', 'views/library_menu.xml', 'views/book_view.xml',  'views/book_list_template.xml'],
 }
